6f3489f2e0d55ed2eb2b365fe19567528b1a2384
Ольга <o-style@bk.ru>
2025-02-21 13:40:37 +0300

    second version of advanced to-do-list. Separate places to set task as done and editing: for check action created checkbox (unicode &#9744;) on the left side; for edit action double click on task text; to save new text click a button on the right (shows only when editing).

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-window.addEventListener('load', () =>{
+window.addEventListener('load', () => {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
 
-    form.addEventListener('submit', (e) =>{
+    form.addEventListener('submit', (e) => {
         e.preventDefault();
         const task = input.value;
         if (!task) {
@@ -12,6 +12,12 @@ window.addEventListener('load', () =>{
         }
         const task_el = document.createElement('div');
         task_el.classList.add("task");
+
+        const task_image_el = document.createElement("span");
+        task_image_el.classList.add("span");
+        task_image_el.innerHTML = '&#9744;';
+        task_el.appendChild(task_image_el);
+
         const task_content_el = document.createElement("li");
         task_content_el.classList.add("content");
         task_el.appendChild(task_content_el);
@@ -25,29 +31,35 @@ window.addEventListener('load', () =>{
         task_actions_el.classList.add("actions");
         const task_edit_el = document.createElement("button");
         task_edit_el.classList.add("edit");
-        task_edit_el.innerHTML = "edit";
+        task_edit_el.innerHTML = "";
         const task_delete_el = document.createElement("button");
         task_delete_el.classList.add("delete");
-        task_delete_el.innerHTML = "delete";
+        task_delete_el.innerHTML = "удалить";
         task_actions_el.appendChild(task_edit_el);
         task_actions_el.appendChild(task_delete_el);
         task_el.appendChild(task_actions_el);
         list_el.appendChild(task_el);
         input.value = "";
 
-        task_content_el.addEventListener('dblclick', () => {
-            if (task_edit_el.innerText.toLowerCase() == "edit") {
-                task_input_el.removeAttribute("readonly");
-                task_input_el.focus();
-                task_edit_el.innerText = 'save';
-            } else {
-                task_input_el.setAttribute("readonly", "readonly");
-                task_edit_el.innerText = 'edit';
-            }
+        task_input_el.addEventListener('dblclick', () => {
+            task_input_el.removeAttribute("readonly");
+            task_input_el.focus();
+            task_edit_el.innerText = 'сохранить';
+            task_edit_el.style.opacity = '1';
+        })
+        task_edit_el.addEventListener('click', () => {
+            task_input_el.setAttribute("readonly", "readonly");
+            task_edit_el.innerText = '';
+            task_edit_el.style.opacity = '0';
         })
 
-        task_content_el.addEventListener('click', () => {
-            task_content_el.classList.toggle("checked")
+        task_image_el.addEventListener('click', () => {
+            task_content_el.classList.toggle("checked");
+            if (task_image_el.innerHTML == '☐') {
+                task_image_el.innerHTML = '&#9745;';
+            } else {
+                task_image_el.innerHTML = '&#9744;';
+            }
         })
 
         task_delete_el.addEventListener('click', () => {
